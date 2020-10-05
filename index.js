cd8012d8740a98095f2139cefcae1b8d3238ed7c
twclark0 <tylerc644@gmail.com>
2020-10-05 10:24:02 -0600

    Add prompt and ask user questions

--- index.js
+++ index.js
@@ -1,17 +1,43 @@
 #!/usr/bin/env node
 
 const clear = require("clear");
-const args = require("minimist")(process.argv.slice(2));
 const opn = require("opn");
+const chalk = require("chalk");
+const figlet = require("figlet");
+const inquirer = require("inquirer");
 
 clear();
 
-const text = args["_"][0];
+console.log(
+  chalk.black(figlet.textSync("Auth0", { horizontalLayout: "full" }))
+);
 
-if (text === "angular") {
-  opn("https://auth0.com/docs/quickstart/spa/angular");
-} else if (text === "react") {
-  opn("https://auth0.com/docs/quickstart/spa/react");
-} else {
-  opn("https://support.auth0.com/");
+function prompt() {
+  const questions = [
+    {
+      name: "support",
+      type: "list",
+      message: "What can we help you with today?",
+      validate: function (value) {
+        if (value.length) {
+          return true;
+        } else {
+          return "Please enter your username or e-mail address.";
+        }
+      },
+      choices: ["support", "react", "angular", "node"],
+      loop: true
+    }
+  ];
+  return inquirer.prompt(questions);
 }
+
+prompt().then(({ support }) => {
+  if (support === "angular") {
+    opn("https://auth0.com/docs/quickstart/spa/angular");
+  } else if (support === "react") {
+    opn("https://auth0.com/docs/quickstart/spa/react");
+  } else {
+    opn("https://support.auth0.com/");
+  }
+});
